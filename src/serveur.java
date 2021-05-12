add9e09fb0c1580cf54b538d8b2afc404f1c6c0f
Hamid <hamidmaouloud4@gmail.com>
2021-05-13 00:32:52 +0200

    serveur

--- src/serveur.java
+++ src/serveur.java
@@ -1,10 +1,12 @@
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.SocketException;
+import java.util.ArrayList;
 
 /**
 *
@@ -15,7 +17,7 @@ import java.net.SocketException;
 public class serveur extends Thread {
 	
 	
-	volatile int difficulte = 3;
+	volatile static int difficulte = 3;
 	volatile static blockchain blockchaine;
 	ServerSocket transactionServeur;
 	
@@ -26,6 +28,7 @@ public class serveur extends Thread {
 	private final int portTransaction=5052;
 	
 	 private final int portmulticast = 5053;
+	 volatile static ArrayList<Socket> mineur = new ArrayList<>();
 	 public ServerSocket multicastSocket;
 	 
 	 
@@ -52,7 +55,6 @@ public class serveur extends Thread {
 		super.run();
 	}
 	
-	
 	public class initTask extends Thread {
 		
 		private Socket socket;
@@ -63,8 +65,11 @@ public class serveur extends Thread {
 		}
 		
 	public void run() {
-			
+					
 		try {
+				this.socket.setSoTimeout(100);
+				socket = initSocket.accept();
+				System.out.println("Un nouveau mineur demande l'etat...");
 				OutputStream o = socket.getOutputStream();
 				ObjectOutputStream os=new ObjectOutputStream(o);
 				os.writeObject(serveur.blockchaine);
@@ -74,14 +79,18 @@ public class serveur extends Thread {
 				
 				e.printStackTrace();
 			}
+			finally {
+	            try {
+	                socket.close();
+	            } catch (IOException ex) {
+	        }
 			
-			super.run();
 		}
 	}
 	public class TransactionTask extends Thread{
 		
 		Socket socket;
-
+		RequeteTransaction transactionRequest;
 		
 		public TransactionTask(Socket socket) {
 			super();
@@ -89,20 +98,77 @@ public class serveur extends Thread {
 		}
 		
 		public void run() {
+			 blockchain chaine = serveur.blockchaine;
+		     boolean valid = false;
 			 try {
-				socket.setSoTimeout(1000);
-				OutputStream os=socket.getOutputStream();
-				//ObjectInputStream is = new ObjectInputStream();
-				
+					socket.setSoTimeout(1000);
+					InputStream os=socket.getInputStream();
+					ObjectInputStream is = new ObjectInputStream(os);
+		            transactionRequest = (RequeteTransaction) is.readObject();
+					 
+		            chaine.addBlock(transactionRequest.transaction, transactionRequest.sel,difficulte);
+		            //serveur.nextDifficulte();
+		            valid = true;
+		            
 			} catch (SocketException e) {
 				
 				e.printStackTrace();
+				
 			} catch (IOException e) {
 				System.err.println("Le mineur n'est pas correct!!");
 				e.printStackTrace();
+			} catch (ClassNotFoundException e) {
+				e.printStackTrace();
 			}
-			super.run();
-		}
+		     if (!socket.isClosed()) {
+		            try {
+		                ObjectOutputStream os = new ObjectOutputStream(socket.getOutputStream());
+		                if (valid) {
+		                    ReponseTransaction reponse = new ReponseTransaction(transactionRequest.transaction, transactionRequest.sel, serveur.difficulte, true);
+		                   // serveur.multicast(reponse); 
+		                    os.writeObject(reponse);
+		                    os.close();
+
+		                } else {
+		                    ReponseTransaction response = new ReponseTransaction(false);
+		                    os.writeObject(response);
+		                    os.close();
+		                }
+		            } catch (IOException ex) {
+		            	ex.printStackTrace();
+		            }
+
+		            try {
+		                socket.close();
+		            } catch (IOException ex) {
+		            	ex.printStackTrace();
+		            }
+		        }
+
+		    }
 		
+		}
+	public class MulticastTask extends Thread {
+		 
+		ReponseTransaction response;
+		
+		public MulticastTask(ReponseTransaction response) {
+			super();
+			this.response = response;
+		}
+
+		@Override
+		public void run() {
+			  serveur.mineur.forEach((mineur) -> {
+		            try (ObjectOutputStream os = new ObjectOutputStream(mineur.getOutputStream())) {
+		                os.writeObject(response);
+		                os.flush();
+		            } catch (IOException ex) {
+		                ex.printStackTrace();
+		            }
+		        });
+		    }
+		}
 	}
+	
 }
