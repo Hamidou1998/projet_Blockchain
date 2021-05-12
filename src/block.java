39aed108c88ea00b290a647f39d62c8e51019750
Hamid <hamidmaouloud4@gmail.com>
2021-05-13 00:36:28 +0200

    structure block

--- src/block.java
+++ src/block.java
@@ -8,6 +8,13 @@ public class block {
 	private Etat etatFinal;
 	private Transaction transactionsEffectuee;
 	
+	
+	public block(Etat etatFinal, Transaction transactionsEffectuee) {
+		super();
+		this.etatFinal = etatFinal;
+		this.transactionsEffectuee = transactionsEffectuee;
+	}
+	
 	public Etat getEtatFinal() {
 		return etatFinal;
 	}
