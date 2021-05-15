a08ee71c2a51643721ac5b7ab691cba4617f3ad5
Hamid <hamidmaouloud4@gmail.com>
2021-05-15 23:28:27 +0200

    TRansaction reponse

--- src/ReponseTransaction.java
+++ src/ReponseTransaction.java
@@ -1,21 +1,28 @@
 import java.io.Serializable;
+
 /**
 *
 * @author Fantome_Hamidou
 * 
 */
+
 public class ReponseTransaction implements Serializable{
 	
 	public Transaction transaction;
 	public int sel;
 	public int difficulte;
+	public boolean accept;
 	
-	
-	public ReponseTransaction(Transaction transaction, int sel, int difficulte) {
+
+    public ReponseTransaction(boolean accept) {
+        this.accept = accept;
+    }
+	public ReponseTransaction(Transaction transaction, int sel, int difficulte,boolean accpet) {
 		super();
 		this.transaction = transaction;
 		this.sel = sel;
 		this.difficulte = difficulte;
+		this.accept=accept;
 	}
 	
 	
